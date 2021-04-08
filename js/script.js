9b1ed3956092e270cbed97a4f31fbed6326696b8
Alexey Mironov <flamurey@gmail.com>
2021-04-08 23:53:03 +0300

    Add goods in cart(start work)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    //-------------------------------Authentication---------------------------
+
+    const headerCart = document.querySelector('.header__cart');
+    const headerLogin = document.querySelector('.header__login');
     const inputEmail = document.querySelectorAll('.header__login_input')[0];
     const formLogin = document.forms[0];
     const form = document.getElementById('registration-form');
     const container = document.getElementById('registration-form-container');
-    const API = `http://localhost:3000/users`;
+    const API = `http://localhost:3000/`;
+    let spanCart = document.createElement('span');
+    let pTotal = document.createElement('p');
+    pTotal.style.fontWeight = 'bold';
+    headerCart.append(spanCart);
+    headerCart.append(pTotal);
+
+    function changeCartBlock(count = 0, total = 0) {
+        spanCart.innerHTML = `<a href="cart.html"><i class="fas fa-shopping-cart"> ${count} items</i></a>`;
+        pTotal.innerHTML = `Total: &euro;${total}`;
+    }
+
+    changeCartBlock();
+
+    checkLoggied();
 
     function checkEmailField() {
         if (!this.value.includes('@')) {
@@ -17,13 +36,12 @@ document.addEventListener('DOMContentLoaded', () => {
     inputEmail.onblur = checkEmailField;
 
     async function checkEmailInDb(email) {
-        let response = await fetch(`${API}?email=${email}`);
+        let response = await fetch(`${API}users?email=${email}`);
         return await response.json();
     }
 
     formLogin.addEventListener('submit', (e) => {
         e.preventDefault();
-
         let email = formLogin.elements.login.value;
         let password = formLogin.elements.password.value;
 
@@ -32,18 +50,114 @@ document.addEventListener('DOMContentLoaded', () => {
             return false;
         }
 
-        checkEmailInDb(email).then(user => {
-            if (user.length === 0) {
+        checkEmailInDb(email).then(users => {
+            if (users.length === 0) {
                 alert('User with this E-mail does not exist');
             } else {
-                if (user[0].password === password) {
-                    alert('You are authenticated!!!');
+                if (users[0].password === password) {
+                    (async () => {
+                        displayLogoutBlock(users[0].email, users[0].id);
+                        await fetch(`${API}users/${users[0].id}`, {
+                            method: 'PATCH',
+                            headers: {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ isLoggied: true })
+                        });
+                    })();
+                    document.cookie = `email=${users[0].email}`;
+                    formLogin.elements.password.textContent = '';
                 } else {
                     alert('Wrong password entered');
                 }
             }
         });
     })
+
+    function readCookie(name) {
+        const searchName = name + "=";
+        const cookies = document.cookie.split(";");
+
+        for (let i = 0; i < cookies.length; i++) {
+            let c = cookies[i];
+
+            while (c.charAt(0) === " ") {
+                c = c.substring(1, c.length);
+            }
+
+            if (c.indexOf(searchName) === 0) {
+                return c.substring(searchName.length, c.length);
+            }
+        }
+
+        return null;
+    }
+
+    function deleteCookie(name) {
+        const domain = window.location.hostname,
+            path = '/'; // root path
+
+        document.cookie = [
+            name, '=',
+            '; expires=' + new Date(0).toUTCString(),
+            '; path=' + path,
+            '; domain=' + domain
+        ].join('');
+    }
+
+    function displayLogoutBlock(login, id) {
+        headerLogin.style.display = 'none';
+        let div = document.createElement('div');
+        let span = document.createElement('span');
+        let btn = document.createElement('button');
+        div.classList.add('header__logout');
+        div.style.display = 'block';
+        span.textContent = `${login}`;
+        btn.textContent = 'Logout';
+        div.append(span);
+        div.append(btn);
+        headerCart.before(div);
+
+        btn.addEventListener('click', () => {
+            deleteCookie('email');
+            (async () => {
+                await fetch(`${API}users/${id}`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ isLoggied: false })
+                });
+            })();
+            displayLoginBlock();
+        });
+    }
+
+    function displayLoginBlock() {
+        let headerLogout = document.querySelector('.header__logout');
+        headerLogout.style.display = 'none';
+        headerLogin.style.display = '';
+    }
+
+    function checkLoggied() {
+        if (document.cookie.indexOf("email") !== -1) {
+            let login = readCookie("email");
+
+            checkEmailInDb(login).then(users => {
+                const [user] = users;
+                if (typeof user != "undefined"
+                    && user.email === login
+                    && user.isLoggied === true) {
+                    displayLogoutBlock(user.email, user.id);
+                }
+            });
+        }
+    }
+
+
+    // -------------------------Registration------------------------
 
     function showCover() {
         let coverDiv = document.createElement('div');
@@ -81,7 +195,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
             async function postUserData(data) {
-                let response = await fetch(API, {
+                let response = await fetch(`${API}users`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json;charset=utf-8'
@@ -134,5 +248,57 @@ document.addEventListener('DOMContentLoaded', () => {
                 return false;
             }
         };
+    });
+
+
+//-------------------------Goods Grid----------------------------------------
+
+    let cart = [];
+    let total = 0;
+    const goodsGrid = document.getElementById('grid');
+
+    async function getGoods() {
+        let response = await fetch(`${API}goods`);
+        return await response.json();
+    }
+
+    getGoods().then(goods => {
+        goods.map(item => {
+            let div = document.createElement('div');
+            div.classList.add('goods_item');
+            let imageItem = document.createElement('img');
+            imageItem.setAttribute('src', item.image);
+            imageItem.setAttribute('alt', item.title);
+            let titleItem = document.createElement('span');
+            titleItem.textContent = `${item.title}`;
+            let priceItem = document.createElement('p');
+            priceItem.innerHTML = `Price: &euro;${item.price}`;
+            let btnAddCart = document.createElement('button');
+            btnAddCart.textContent = 'Add to Cart';
+            div.append(imageItem);
+            div.append(titleItem);
+            div.append(priceItem);
+            div.append(btnAddCart);
+            goodsGrid.append(div);
+
+            btnAddCart.addEventListener('click', () => {
+                cart = [...cart, item];
+                total += item.price;
+                changeCartBlock(cart.length, total);
+                console.log(cart);
+                console.log(total);
+
+                (async () => {
+                    await fetch(`${API}users/1`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ cart })
+                    });
+                })();
+            })
+        });
     });
 });
